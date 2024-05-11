6d5525c3de7d774b175edb6371f776fa1242ad68
Roisull <lusiro2001@students.ac.id>
2024-05-11 11:00:21 +0700

    membuat NoteViewModel, NoteViewModelFactory, and implementasi di MainActivity

--- app/src/main/java/com/roisul/jaran/MainActivity.kt
+++ app/src/main/java/com/roisul/jaran/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.roisul.jaran.database.NoteDatabase
+import com.roisul.jaran.repository.NoteRepository
+import com.roisul.jaran.viewmodel.NoteViewModel
+import com.roisul.jaran.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
--- app/src/main/java/com/roisul/jaran/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/roisul/jaran/viewmodel/NoteViewModel.kt
@@ -1,4 +1,31 @@
 package com.roisul.jaran.viewmodel
 
-class NoteViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.roisul.jaran.model.Note
+import com.roisul.jaran.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app: Application, private val noteRepository: NoteRepository): AndroidViewModel(app) {
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+
+    fun updateNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updatetNote(note)
+        }
+
+    fun getAllNotes() = noteRepository.getAllNote()
+
+    fun searchNote(query: String?) =
+        noteRepository.searchNote(query)
 }
