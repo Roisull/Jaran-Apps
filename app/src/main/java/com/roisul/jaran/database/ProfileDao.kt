b66f2fe0876f59884d6b5339a86b2c285ba7ede6
Roisull <lusiro2001@students.ac.id>
2024-05-12 10:58:55 +0700

    membuat model profile

---
+++ app/src/main/java/com/roisul/jaran/database/ProfileDao.kt
@@ -0,0 +1,4 @@
+package com.roisul.jaran.database
+
+interface ProfileDao {
+}
--- app/src/main/java/com/roisul/jaran/model/Profile.kt
+++ app/src/main/java/com/roisul/jaran/model/Profile.kt
@@ -1,3 +1,16 @@
 package com.roisul.jaran.model
 
-data class Profile()
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "profiles")
+@Parcelize
+data class Profile(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val profileName: String,
+    val profileSocMed: String,
+    val profileImageUri: String
+): Parcelable
